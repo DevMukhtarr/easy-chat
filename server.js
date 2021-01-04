becc653e3477fe422be0f9d3053958143d9bbb6d
Your Name <you@example.com>
2021-01-04 15:02:49 +0100

    added port

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 'use strict';
 // const http = require('http')
-const PORT = 8000;
+const PORT = 5000;
 const express = require('express');
 const app = express();
 const myApp = require('./myApp');
+require('dotenv').config();
 
 
 app.use(express.static(__dirname))
@@ -11,6 +12,6 @@ app.use(myApp)
 app.get('/', (req, res) =>{
   res.sendFile(__dirname + '/view/index.html')
 })
-app.listen(PORT,() => {
+app.listen(process.env.PORT || PORT,() => {
   console.log(`Listening on port ${PORT}!`);
 });
