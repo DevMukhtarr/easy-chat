5f94990a2e7685060ec8e0977a64eebc237ad7f8
Your Name <you@example.com>
2021-01-17 14:19:19 +0100

    improved ui and backend technologies

--- myApp.js
+++ myApp.js
@@ -4,16 +4,23 @@ const bodyParser = require('body-parser');
 require('dotenv').config()
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const passport = require('passport');
+const session = require('express-session');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const ObjectID = require('mongodb').ObjectID;
 
+
+app.set('views', './view');
+app.set('view engine', 'pug');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 //A schema for the Data i'll be receiving
 const usersSchema = new Schema({
-    nickname: String,
+    username: String,
     email: String,
-    password: String,
-    confirmPassword: String 
+    password: String
 })
 
 const User = mongoose.model('User', usersSchema);
@@ -25,52 +32,84 @@ app.use(bodyParser.urlencoded({
 
 //middleWare that Parses Json
 app.use(bodyParser.json())
+ 
+//taking to register page
+app.get('/register', (req, res) =>{
+    res.render('registration')
+})
+ 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: true}
+}))
 
-app.get('/registration', (req, res) =>{
-    res.sendFile(__dirname + '/view/registration.html');
-    // console.log(process.env.MONGO_URI)
-})    
-
-app.get('/login', (req, res) =>{
-    res.sendFile(__dirname + '/view/index.html')
-})    
-
-app.post('/login', (req, res) =>{
-    let username = req.body.nickname;
-    let password = req.body.password;
-    res.json({username: username, password: password})
+//serialize user with passport
+passport.serializeUser((user, done) =>{
+    done(null, user._id)
 })
 
-app.post('/register', (req, res) =>{
-    let username = req.body.nickname;
-    let email = req.body.email;
-    let password = req.body.password;
-    let confirmPassword = req.body.confirmPassword;
-
-    //model to be sent to db
-    let userReg = new User({
-        nickname: username,
-        email: email,
-        password: password,
-        confirmPassword: confirmPassword
+//deserilize user with passport
+passport.deserializeUser((id, done) =>{
+    User.findOne({_id: new ObjectID(id)}, (err, doc) =>{
+        done(null, doc)
     })
-
-    userReg.save(
-        (error, data) =>{
-        if(error){
-            console.log(error)
-        }else{
-            return data
-        }
-    }
-    )
-    let directHome = () =>{
-        return res.sendFile(__dirname + '/view/index.html')
-            // res.sendFile(__dirname + '/view/index.html')
-    }
-    setTimeout(directHome, 2000);
 })
 
+passport.use(new LocalStrategy((username,password, done) =>{
+        User.findOne({username: username}, (err, user) =>{
+            if(err){
+                return done(err)
+            }
+            if(!user){
+                return done(null, false)
+            }
+            if(!bcrypt.compareSync(password, user.password)){
+                return done(null, false)
+            }
+            return done(null, user)
+        })
+}
+))
+
+//to register new users
+app.post('/register', (req,res, next) =>{
+    User.findOne({email: req.body.email}, (err, user) =>{
+        if(err){
+            next(err)
+        }else if(user){
+            //this invalid is referres to when the user is registered already
+            let invalid = "The email is a registered user"
+            res.render(process.cwd() + '/view/registration', {invalid: invalid})
+        }else{
+            //this invalid is referred to when password and confirm password does not match
+            let invalid = "Password and confirm password does not match"
+            if(req.body.password !== req.body.confirmPassword){
+                res.render(process.cwd() + '/view/registration', {invalid: invalid})
+            }else{
+                const hash = bcrypt.hashSync(req.body.password, 12);
+
+                //creating data to save in db 
+                let ourUser = new User({
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: hash
+                }).save((error, data) =>{
+                    if(error){
+                        res.redirect('/')
+                    }else{
+                        return data
+                    }
+                })
+
+                res.render(process.cwd() + '/view/login-success', {username: req.body.username})
+            }
+        }
+    })
+})
+
+//to login new users
 
 
 
--- server.js
+++ server.js
@@ -2,16 +2,22 @@
 // const http = require('http')
 const PORT = 5000;
 const express = require('express');
-const app = express();
 const myApp = require('./myApp');
 require('dotenv').config();
+const app = express();
 
-
-app.use(express.static(__dirname))
 app.use(myApp)
+app.set('views', './view')
+app.set('view engine', 'pug')
+//accessing the defaalt home styles
+app.use('/styles', express.static(process.cwd() + '/styles'))
+
+//routing to the home page which is login
 app.get('/', (req, res) =>{
-  res.sendFile(__dirname + '/view/index.html')
+  res.render('index')
 })
+
+//server listen
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Listening on port ${PORT}!`);
 });
