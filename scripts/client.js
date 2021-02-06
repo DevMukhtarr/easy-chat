b8beb0c8e1cbfe29af5c6b1b71e30198d6267102
Your Name <you@example.com>
2021-02-06 17:56:05 +0100

    new branch update

--- scripts/client.js
+++ scripts/client.js
@@ -3,6 +3,6 @@
     const socket  = io();
 
     socket.on('users', (data) =>{
-        $('#num-users').text(data.currentUsers > 1 ?data.currentUsers + ' users' + ' are online' 
+        $('#num-users').text(data.currentUsers > 1 ? data.currentUsers + ' users' + ' are online' 
         : data.currentUsers +' user' + ' is online');
     })
