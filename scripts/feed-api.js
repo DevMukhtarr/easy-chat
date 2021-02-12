dcc2f98570a926e321122419e8fb7797f16f1751
Your Name <you@example.com>
2021-02-12 12:40:26 +0100

    added feed-api.js file

---
+++ scripts/feed-api.js
@@ -0,0 +1,24 @@
+    let selector = () =>{
+    let searches = ['house','device','women','nature','men','animals','cartoon','tesla'];
+    let randomNum = Math.floor(Math.random() * searches.length);
+        console.log(searches[randomNum])
+        return searches[randomNum];
+    }
+    let selectorUp = selector()
+
+
+const unsplashUrl = "https://api.unsplash.com/photos/?query="+selectorUp+"&client_id=I9Igs7TZrAfVAWnvNEugshgquPJUJUPdQo1d1t01wD8"
+
+let photoContainer = document.querySelector('.photo-container');
+
+fetch(unsplashUrl)
+.then(info => info.json())
+.then(data =>{
+    for(let i = 0; i < 10; i++){
+            let image = document.createElement('img')
+            image.setAttribute('src', data[i].urls.small);
+            image.setAttribute('loading', 'lazy')
+            photoContainer.appendChild(image)
+            console.log(data[i].urls.raw)
+        }
+    })
